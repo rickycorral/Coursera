8d89331042d81de02e44eaeb291134e8e617aa6f
rickycorral <52468067+rickycorral@users.noreply.github.com>
2023-10-01 14:18:32 -0600

    Update script.js

--- script.js
+++ script.js
@@ -4,52 +4,49 @@ function playDrumSound(buttonId, soundId) {
   const sound = document.getElementById(soundId);
 
   button.addEventListener('click', () => {
-      // Check if the audio is currently playing; if so, reset it to the beginning
-      if (!sound.paused) {
-          sound.currentTime = 0;
-      }
+    // Check if the audio is currently playing; if so, reset it to the beginning
+    if (!sound.paused) {
+      sound.currentTime = 0;
+    }
 
-      // Play the drum sound
-      sound.play();
+    // Play the drum sound
+    sound.play();
   });
 }
 
 // Function to play the kick sound when "a" key is pressed
-function playKickOnKeyPress(event) {
+// or the snare sound when "s" key is pressed
+function playDrumOnKeyPress(event) {
   if (event.key === 'a' || event.key === 'A') {
-      // Trigger the "kick" sound
-      const kickSound = document.getElementById('kick-sound');
-      
-      // Check if the audio is currently playing; if so, reset it to the beginning
-      if (!kickSound.paused) {
-          kickSound.currentTime = 0;
-      }
-      
-      // Play the kick drum sound
-      kickSound.play();
-  }
-}
+    // Trigger the "kick" sound
+    const kickSound = document.getElementById('kick-sound');
 
-function playSnareOnKeyPress(event) {
-  if (event.key === 's' || event.key === 'S') {
-      // Trigger the "snare" sound
-      const snareSound = document.getElementById('snare-sound');
-      
-      // Check if the audio is currently playing; if so, reset it to the beginning
-      if (!snareSound.paused) {
-          snareSound.currentTime = 0;
-      }
-      
-      // Play the kick drum sound
-      Sound.play();
+    // Check if the audio is currently playing; if so, reset it to the beginning
+    if (!kickSound.paused) {
+      kickSound.currentTime = 0;
+    }
+
+    // Play the kick drum sound
+    kickSound.play();
+  } else if (event.key === 's' || event.key === 'S') {
+    // Trigger the "snare" sound
+    const snareSound = document.getElementById('snare-sound');
+
+    // Check if the audio is currently playing; if so, reset it to the beginning
+    if (!snareSound.paused) {
+      snareSound.currentTime = 0;
+    }
+
+    // Play the snare drum sound
+    snareSound.play();
   }
 }
 
 // Add event listener for keypress events
-document.addEventListener('keypress', playKickOnKeyPress);
-document.addEventListener('keypress', playSnareOnKeyPress);
+document.addEventListener('keypress', playDrumOnKeyPress);
+
 // Usage for the Kick button and sound
 playDrumSound('kick-button', 'kick-sound');
 
-// Usage for the Ride button and sound
+// Usage for the Snare button and sound
 playDrumSound('snare-button', 'snare-sound');
